bf4b36f66069f2b343477dcff8b91f32b11d3365
nandakumar_kadavannoore <nandakumark6693@gmail.com>
2018-02-10 14:12:52 +0530

    Create redirectController.java

---
+++ redirectController.java
@@ -0,0 +1,30 @@
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin
+public SampleClass {
+
+  /**
+	 * This will redirect to given URL or site
+	 */
+	@GetMapping(value = "/redirect")
+	public void oAuthRedirect(HttpServletResponse httpServletRespone,
+                      @RequestParam(value = "state", required = false) String state,
+			                @RequestParam(value = "code", required = false) String code) {
+		StringBuilder redirectURL = new StringBuilder("www.google.co.in");
+		redirectURL.append("?state=");
+		redirectURL.append(state);
+		redirectURL.append("&code=");
+		redirectURL.append(code);
+
+    // Set return status as 302
+		httpServletResponse.setHeader("Location", redirectURL.toString());
+		httpServletResponse.setStatus(302);
+	 }
+  
+ }
